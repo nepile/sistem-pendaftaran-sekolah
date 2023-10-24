4cf53d208133fb773baf653b43c8e9502e8790ca
nepile <nevillejro.laia@gmail.com>
2023-10-25 01:54:57 +0700

    feat: create method to handle many categories input from users

---
+++ src/app/controllers/InputData.java
@@ -0,0 +1,19 @@
+package app.controllers;
+
+import java.util.Scanner;
+
+public class InputData {
+    /**
+     * Method to handle users input
+     * 
+     * @param info
+     * @return data of string
+     */
+    public static String input(String info) {
+        System.out.print(info + " >> ");
+        Scanner userInput = new Scanner(System.in);
+        String data = userInput.nextLine();
+
+        return data;
+    }
+}
