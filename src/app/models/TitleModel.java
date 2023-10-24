59fc9c4266821b0336fffa08dd5dfbfa3edaf5df
nepile <nevillejro.laia@gmail.com>
2023-10-25 02:39:13 +0700

    refac: update modifier of path title model

--- src/app/models/TitleModel.java
+++ src/app/models/TitleModel.java
@@ -1,7 +1,7 @@
 package app.models;
 
 public class TitleModel {
-    private String path = "D:\\\\Java Journey\\\\sistem-pendaftaran-sekolah\\\\src\\\\app\\\\data\\\\title.txt";
+    String path = "D:\\Java Journey\\sistem-pendaftaran-sekolah\\src\\app\\data\\title.txt";
 
     public String data() {
         return this.path;
