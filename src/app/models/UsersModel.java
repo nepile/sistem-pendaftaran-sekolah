9bce693bed046237850a8a16fa4ac0fb1b2dc8a0
nepile <nevillejro.laia@gmail.com>
2023-10-25 21:12:07 +0700

    model: create a representation data users by users model

---
+++ src/app/models/UsersModel.java
@@ -0,0 +1,23 @@
+package app.models;
+
+public class UsersModel {
+
+    /**
+     * Method to store user admin data
+     * 
+     * @return admin data of multidimensions array
+     */
+    public static String[][] staticDataByAdmin() {
+        String[][] admin = { { "Neville", "12345" }, { "Jeremy", "54321" } };
+        return admin;
+    }
+
+    /**
+     * Method to return users data depends on fillable field data
+     * 
+     * @return users data
+     */
+    public String[][] data() {
+        return staticDataByAdmin();
+    }
+}
